d16d6d297b1e0d750d631af9115baf4d1c8afb13
Avi Wilensky <avi@promediacorp.com>
2014-12-14 18:42:24 -0500

    initial commit

---
+++ fbnames/items.py
@@ -0,0 +1,7 @@
+from scrapy.item import Item, Field
+
+class FbnamesItem(Item):
+	anchor = Field()
+	link = Field()
+
+    
---
+++ fbnames/spiders/spidey.py
@@ -0,0 +1,43 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spider import BaseSpider
+#from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from fbnames.items import FbnamesItem
+from scrapy.contrib.linkextractors import LinkExtractor
+
+class MySpider(CrawlSpider):
+    name = "fb"
+    allowed_domains = ["uphail.com"]
+    start_urls = ["http://www.uphail.com"]
+
+    # rules = (Rule (LxmlLinkExtractor(allow=(r"",) deny=())
+    # , callback="parseo", follow= False),
+    # )
+
+    rules = (
+
+        Rule(
+            LinkExtractor(allow=('',),
+
+                          deny=('vt/',
+                          ),
+            ),
+            callback='parse_item',
+            follow=True
+        ),
+
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        urls = hxs.xpath('//a')
+        items = []
+        for url in urls:
+            item = FbnamesItem()
+            item["anchor"] = url.xpath("text()").extract()
+            item["link"] = url.xpath("@href").extract()
+            items.append(item)
+        return items
+
+
+        #crawls
