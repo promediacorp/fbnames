2e8f198c77846966c459ed49ef7d5f3195984f3f
jdelman <joshdelman@gmail.com>
2014-12-18 12:51:39 -0500

    new image

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,1 +1,1 @@
-ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ printf "%s", $1}' | redis-cli -x -h 172.30.0.242 lpush plist
+/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ printf "%s", $1}' | /usr/local/bin/redis-cli -x -h 172.30.0.242 lpush plist
